d89ee2e319f3137fbe788c7bc6563af34a63ad32
Tanvir <tk177277@gmail.com>
2021-10-11 13:23:11 +0600

    comments correction

--- Solidity Task/contracts/Escrow.sol
+++ Solidity Task/contracts/Escrow.sol
@@ -32,8 +32,8 @@ contract Escrow{
     */
     enum CONFIRMATION_STATUS { 
         INACTIVE            ,           // 0, this is the default status 
-        FINALIZE_PAYMENT    ,           // 2, when depositor confirms that recipient can recieve the money
-        PENDING                         // 5, when the deposite is still active but the payment to the recipient is yet pending
+        FINALIZE_PAYMENT    ,           // 1, when depositor confirms that recipient can recieve the money
+        PENDING                         // 2, when the deposite is still active but the payment to the recipient is yet pending
     }
     
     /*
