68f77bc02a315e5c7ad4394d005b4ab4af5ed8d5
Tanvir <tk177277@gmail.com>
2021-10-12 17:51:13 +0600

    updated the web3 caller

--- Web3 Client Task/EscrowCaller.js
+++ Web3 Client Task/EscrowCaller.js
@@ -8,127 +8,260 @@ async function getContract(){
 } 
 
 async function getAccountBalance(accountAddress){
-    let balance = await web3.eth.getBalance(accountAddress);
-    console.log(balance);
-    return balance;
+    let resonseObject ={
+        
+    }
+    
+    try{
+        let balance = await web3.eth.getBalance(accountAddress);
+        resonseObject.response_status   = "success";
+        resonseObject.message           = "the balance was successfully retrieved"
+        resonseObject.detailed_response = balance;
+    }
+    catch(err){
+        resonseObject.response_status   = "failure";
+        resonseObject.message           = err.data
+        resonseObject.detailed_response = err
+    }
+    
+    console.log(resonseObject);
+
+    return resonseObject;
 }
 // getAccountBalance("0x7e5f519016277434da984e820b36578d62a3885e");
 
 async function getNonceForAddress(address){
-    let nonce = await web3.eth.getTransactionCount(address);
-    console.log(nonce);
-    return nonce;
+    
+    let resonseObject ={
+       
+    }
+    try{
+        
+        let nonce = await web3.eth.getTransactionCount(address);
+        
+        resonseObject.response_status   = "success";
+        resonseObject.message           = ""
+        resonseObject.detailed_response = nonce
+
+        // console.log(nonce);
+    
+    }catch(err){
+        
+        resonseObject.response_status   = "failure";
+        resonseObject.message           = err.data;
+        resonseObject.detailed_response = err;
+    
+    }
+    console.log(resonseObject);
+    return resonseObject;
 }
 // getNonceForAddress("0x7e5f519016277434da984e820b36578d62a3885e");
-
-//======================> contract function caller
 
 async function createDeposite(  depositorAddress, 
                                 recipeintAddress, 
                                 amount,
                                 ){
+    let resonseObject ={
+        
+    }
     try{
-            let contractObj = await getContract();
-            let reciept = await contractObj.methods.depositeCreate(recipeintAddress).send({ 
-                                from: depositorAddress,
-                                value: amount,
-                                gasLimit: "300000"
-                            });
-            console.log(reciept);
-            return reciept;
+        let contractObj = await getContract();
+        let reciept = await contractObj.methods.depositeCreate(recipeintAddress).send({ 
+                            from: depositorAddress,
+                            value: amount,
+                            gasLimit: "300000"
+                        });
+        resonseObject.response_status   = "success";
+        resonseObject.message           = ""
+        resonseObject.detailed_response = reciept;
+        
     }
     catch(err){
-        console.log(Object.values(err.data)[0].reason);
-        return err.data;
+
+        resonseObject.response_status   = "failure";
+        resonseObject.message           = Object.values(err.data)[0].reason
+        resonseObject.detailed_response = err.data;
+        
     }
+    console.log(resonseObject);
+    return resonseObject;
 }
-// createDeposite("0x7e5f519016277434da984e820b36578d62a3885e","0xbd9caec906414d0691fa0ebf1dd51c0c6fdc38af","2000000000000000000")
+// createDeposite("0xee46cbafdd18a8d8e43894fcb29bca48589f004b","0xbd9caec906414d0691fa0ebf1dd51c0c6fdc38af","2000000000000000000")
 
 async function confirmServiceDelivery(depositorAddress){
+    
+    let resonseObject ={
+        
+    }
+    
     try{
         let contractObj = await getContract();
         let reciept = await contractObj.methods.confirmServiceDelivery().send({
                             from : depositorAddress,
                             gasLimit: "300000"
-                        });
-        console.log(reciept);
-        return reciept;
-    }catch(err){
-        console.log(Object.values(err.data)[0].reason);
-        return err.data;
-    }
-}
-// confirmServiceDelivery("0x7e5f519016277434da984e820b36578d62a3885e");
+                        });  
+        
+        resonseObject.response_status   = "success";
+        resonseObject.message           = "";
+        resonseObject.detailed_response = reciept;
 
-async function unlockServiceDelivery(arbitrorAddress, depositorAddress){
+    }catch(err){
+        
+        resonseObject.response_status   = "failure";
+        resonseObject.message           = Object.values(err.data)[0].reason
+        resonseObject.detailed_response = err.data
+    
+    }
+
+    console.log(resonseObject)
+    return resonseObject;
+}
+// confirmServiceDelivery("0xee46cbafdd18a8d8e43894fcb29bca48589f004b");
+
+async function unlockDeposite(arbitrorAddress, depositorAddress){
+    
+    let resonseObject ={
+        
+    }
+    
     try{
         let contractObj = await getContract();
         let reciept = await contractObj.methods.unlockDeposite(depositorAddress).send({
                             from : arbitrorAddress,
                             gasLimit: "300000"
                         })
-        console.log(reciept);
-        return reciept;
+
+        resonseObject.response_status   = "success";
+        resonseObject.message           = ""
+        resonseObject.detailed_response = reciept;
+
     }catch(err){
-        console.log(Object.values(err.data)[0].reason);
-        return err.data;
+        
+        resonseObject.response_status   = "failure";
+        resonseObject.message           = Object.values(err.data)[0].reason
+        resonseObject.detailed_response = err.data
+    
     }
+    console.log(resonseObject);
+    return resonseObject;
 }
-// unlockServiceDelivery("0xf6a9bce0c5cab5f1b079089a13d230a7ba99e154","0x7e5f519016277434da984e820b36578d62a3885e");
+// unlockDeposite("0xf6a9bce0c5cab5f1b079089a13d230a7ba99e154","0xee46cbafdd18a8d8e43894fcb29bca48589f004b");
 
 async function withdrawDeposite(depositorAddress){
+    
+    let resonseObject ={
+        
+    }
+
     try{
         let contractObj = await getContract();
         let reciept = await contractObj.methods.withdrawDeposite().send({
                             from: depositorAddress,
                             gasLimit: "300000"
                         })
-        console.log(reciept);
-        return reciept;
+        
+
+        resonseObject.response_status   = "success";
+        resonseObject.message           = ""
+        resonseObject.detailed_response = reciept;
+
     }catch(err){
-        console.log(Object.values(err.data)[0].reason);
-        return err.data;
+        
+  
+        resonseObject.response_status   = "failure";
+        resonseObject.message           = Object.values(err.data)[0].reason
+        resonseObject.detailed_response = err.data
+    
     }
+    console.log(resonseObject)
+    return resonseObject;
 }
-// withdrawDeposite("0x7e5f519016277434da984e820b36578d62a3885e");
+// withdrawDeposite("0xbd9caec906414d0691fa0ebf1dd51c0c6fdc38af");
 
 async function getDepositStatus(depositorAddress){
+    
+    let resonseObject ={
+        
+    }
+
     try{
         let contractObj = await getContract();
         let reciept = await contractObj.methods.getDepositStatus(depositorAddress).call();
-        console.log(reciept);
-        return reciept;
+        
+        resonseObject.response_status   = "success";
+        resonseObject.message           = ""
+        resonseObject.detailed_response = reciept;
+
     }catch(err){
-        console.log(Object.values(err.data)[0].reason);
-        return err.data;
+        resonseObject.response_status   = "failure";
+        resonseObject.message           = Object.values(err.data)[0].reason
+        resonseObject.detailed_response = err.data
     }
+    console.log(resonseObject);
+    return resonseObject;
 }
-// getDepositStatus("0x7e5f519016277434da984e820b36578d62a3885e");
+// getDepositStatus("0xee46cbafdd18a8d8e43894fcb29bca48589f004b");
 
 async function getContractBalance(){
+    
+    let resonseObject ={
+        
+    }
+    
     try{
         let contractObj = await getContract();
         let reciept = await contractObj.methods.getContractBalance().call();
-        console.log(reciept);
-        return reciept;
+
+        resonseObject.response_status   = "success";
+        resonseObject.message           = ""
+        resonseObject.detailed_response = reciept;
     }catch(err){
-        console.log(Object.values(err.data)[0].reason);
-        return err.data;
+        
+
+        resonseObject.response_status   = "failure";
+        resonseObject.message           = Object.values(err.data)[0].reason
+        resonseObject.detailed_response = err
+    
     }
+    console.log(resonseObject)
+    return resonseObject;
 }
 // getContractBalance();
 
 async function getDepositeInfo(depositorAddress){
   
+    let resonseObject ={
+        
+    }
+
     try{
         let contractObj = await getContract();
         let reciept = await contractObj.methods.getDepositInfo(depositorAddress).call();
-        console.log(reciept);
-        return reciept;
+        
+        resonseObject.response_status   = "success";
+        resonseObject.message           = "successfully retrived depositeInfo"
+        resonseObject.detailed_response = reciept;
+
     }catch(err){
-        console.log(Object.values(err.data)[0].reason);
-        return err.data;
+        
+        resonseObject.response_status   = "failure";
+        resonseObject.message           = Object.values(err.data)[0].reason
+        resonseObject.detailed_response = err.data
     }
+    console.log(resonseObject);
+    return resonseObject;
    
 }
 // getDepositeInfo("0x7e5f519016277434da984e820b36578d62a3885e");
+
+module.exports = {
+    getAccountBalance,
+    getNonceForAddress,
+    createDeposite,
+    confirmServiceDelivery, 
+    unlockDeposite, 
+    withdrawDeposite, 
+    getDepositStatus, 
+    getContractBalance, 
+    getDepositeInfo
+}
---
+++ Web3 Client Task/index.js
@@ -0,0 +1,15 @@
+const EscrowCaller = require('./EscrowCaller');
+
+
+// async function callEscrow(){
+//     try{
+//         let response = await EscrowCaller.createDeposite("0xbd9caec906414d0691fa0ebf1dd51c0c6fdc38af","0x9d557ec322490bdc4f218943b7846d8f4fca4626","2000000000000000000")
+//         console.log(response);
+//     }catch(err){
+//         console.log(err);
+//     }
+// }
+
+// callEscrow();
+
+
