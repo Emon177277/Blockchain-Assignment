a34eb14d41be213215e2e8fa059cc5aee032ae1a
Tanvir <tk177277@gmail.com>
2021-10-12 19:05:13 +0600

    express added

--- Web3 Client Task/index.js
+++ Web3 Client Task/index.js
@@ -1,9 +1,15 @@
-const EscrowCaller = require('./EscrowCaller');
+const escrowCaller = require('./EscrowCaller');
+const express = require('express');
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+
+const app = express();
 
 
 // async function callEscrow(){
 //     try{
-//         let response = await EscrowCaller.createDeposite("0xbd9caec906414d0691fa0ebf1dd51c0c6fdc38af","0x9d557ec322490bdc4f218943b7846d8f4fca4626","2000000000000000000")
+//         let response = await escrowCaller.createDeposite("0xbd9caec906414d0691fa0ebf1dd51c0c6fdc38af","0x9d557ec322490bdc4f218943b7846d8f4fca4626","2000000000000000000")
 //         console.log(response);
 //     }catch(err){
 //         console.log(err);
@@ -13,3 +19,25 @@ const EscrowCaller = require('./EscrowCaller');
 // callEscrow();
 
 
+// app.post('/signup', async(req, res, next) => {
+//     try {
+//       await firstThing()
+//     } catch (error) {
+//       return next(error)
+//     }
+  
+//     try {
+//       await secondThing()
+//     } catch (error) {
+//       return next(error)
+//     }
+//   })
+
+app.get('/', async (req, res)=>{
+    res.status(200).send("it works");
+});
+
+const port = 8083;
+app.listen(port,()=>{
+    console.log("server started successfully at port " + port);
+});
